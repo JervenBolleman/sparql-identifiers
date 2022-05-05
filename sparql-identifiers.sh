748f79395164acd1ba44359d4482f97942474394
Jerven Bolleman <jerven.bolleman@sib.swiss>
2022-05-05 12:28:38 +0200

    Increase version number

--- sparql-identifiers.sh
+++ sparql-identifiers.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sparql=$1
-java -jar target/sparql-identifiers-1.2-SNAPSHOT-jar-with-dependencies.jar "$sparql"
+java -jar target/sparql-identifiers-1.3-SNAPSHOT-jar-with-dependencies.jar "$sparql"
